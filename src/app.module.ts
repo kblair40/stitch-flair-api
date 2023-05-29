86bd9cecd1c143038daeac80cc6001cab9b5d363
Kevin Blair <kblair40@gmail.com>
2023-05-29 10:23:41 -0500

    Fix env vars not populating

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestController } from './test/test.controller';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+const pgConfig: PostgresConnectionOptions = {
+  type: 'postgres',
+  port: 5432,
+  host: 'localhost',
+  username: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: 'stitchflair',
+  synchronize: true,
+};
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(pgConfig),
+  ],
   controllers: [AppController, TestController],
   providers: [AppService],
 })
--- src/test/test.controller.ts
+++ src/test/test.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 
+console.log('PASSWORD:', process.env.PG_PASSWORD);
+
 @Controller('test')
 export class TestController {
   @Get()
