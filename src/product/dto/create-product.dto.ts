31a651c81a23ce47a7047dcb47266025f0309789
Kevin Blair <kblair40@gmail.com>
2023-05-29 17:19:11 -0500

    Finish dto

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,14 +1,10 @@
-import { Category } from 'src/category/entities/category.entity';
-
 export class CreateProductDto {
   name: string;
   price: number;
   description?: string;
-  //   category: string;
-  featured: boolean;
-  on_sale: boolean;
-  on_sale_price: number;
+  featured?: boolean;
+  on_sale?: boolean;
+  on_sale_price?: number;
   image_url: string;
   category_id: number;
-  category: Category;
 }
