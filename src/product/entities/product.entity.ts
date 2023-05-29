0a2151d02a79b5601d793f9e158c5d68d826f504
Kevin Blair <kblair40@gmail.com>
2023-05-29 12:49:31 -0500

    Update entity w/ new columns

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,7 +1,8 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToOne } from 'typeorm';
 
 @Entity()
 export class Product {
+  // might need autoincrementing type here...
   @PrimaryGeneratedColumn()
   id: number;
 
@@ -9,5 +10,26 @@ export class Product {
   name: string;
 
   @Column()
+  description: string;
+
+  @Column()
+  price: number;
+
+  @Column()
   category: string;
+
+  @Column()
+  category_id: number;
+
+  @Column()
+  image_url: string;
+
+  @Column({ default: false })
+  featured: boolean;
+
+  @Column({ default: false })
+  on_sale: boolean;
+
+  @Column()
+  on_sale_price: number;
 }
