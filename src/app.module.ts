cf6442447a790efd771bf7a97a1bb86015a79fe9
Kevin Blair <kblair40@gmail.com>
2023-05-29 10:50:04 -0500

    Add skeleton for product resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestController } from './test/test.controller';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import { ProductModule } from './product/product.module';
 
 const pgConfig: PostgresConnectionOptions = {
   type: 'postgres',
@@ -21,10 +23,13 @@ const pgConfig: PostgresConnectionOptions = {
   imports: [
     TypeOrmModule.forRoot({
       ...pgConfig,
-      // entities: []
+      autoLoadEntities: true,
     }),
+    ProductModule,
   ],
   controllers: [AppController, TestController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
---
+++ src/product/entities/product.entity.ts
@@ -0,0 +1,7 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+}
