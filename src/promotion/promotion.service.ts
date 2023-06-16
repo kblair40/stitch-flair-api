26fd1242d396e3e66dfbb16e681a503512bb384c
Kevin Blair <kblair40@gmail.com>
2023-06-16 07:39:21 -0500

    Implement findAll and create methods

--- src/promotion/promotion.service.ts
+++ src/promotion/promotion.service.ts
@@ -1,15 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  BadRequestException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, DataSource } from 'typeorm';
+
+import { Promotion } from './entities/promotion.entity';
 import { CreatePromotionDto } from './dto/create-promotion.dto';
 import { UpdatePromotionDto } from './dto/update-promotion.dto';
 
 @Injectable()
 export class PromotionService {
-  create(createPromotionDto: CreatePromotionDto) {
-    return 'This action adds a new promotion';
+  constructor(
+    @InjectRepository(Promotion) private promoRepository: Repository<Promotion>,
+    private dataSource: DataSource,
+  ) {}
+
+  async create(input: CreatePromotionDto) {
+    const { text } = input;
+    if (!text || typeof text !== 'string' || !text.trim().length) {
+      throw new BadRequestException('Text property cannot be empty');
+    }
+
+    try {
+      const savedPromo = await this.promoRepository.save(input);
+      console.log('\nSaved Promo:', savedPromo, '\n');
+      return savedPromo;
+    } catch (e) {
+      console.log('Failed to save promotion:', e);
+      throw new InternalServerErrorException(JSON.stringify(e));
+    }
   }
 
-  findAll() {
-    return `This action returns all promotion`;
+  async findAll() {
+    const allPromos = await this.promoRepository.find();
+    console.log('\nallPromos:', allPromos, '\n');
+    return allPromos;
   }
 
   findOne(id: number) {
