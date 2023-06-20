0715a0f7b413081fab4547f929e113fcec8e999b
Kevin Blair <kblair40@gmail.com>
2023-06-20 05:07:25 -0500

    Add entity definition

--- src/text/entities/text.entity.ts
+++ src/text/entities/text.entity.ts
@@ -1,1 +1,25 @@
-export class Text {}
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Text {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: true })
+  homeTitle: string;
+
+  @Column({ nullable: true })
+  homeText: string;
+
+  @CreateDateColumn()
+  created_time: Date;
+
+  @UpdateDateColumn()
+  updated_time: Date;
+}
