1ba82a22e83c0ce6cfefb8ce0d465b71208d3602
Kevin Blair <kblair40@gmail.com>
2023-07-03 06:52:25 -0500

    Add logs for db info

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,8 @@ import { CategoryModule } from './category/category.module';
 import { PromotionModule } from './promotion/promotion.module';
 import { TextModule } from './text/text.module';
 
+console.log('\n\nprocess.env.PG_USER:', process.env.PG_USER);
+console.log('process.env.PG_PASSWORD:', process.env.PG_PASSWORD);
 const pgConfig: PostgresConnectionOptions = {
   type: 'postgres',
   port: 5432,
