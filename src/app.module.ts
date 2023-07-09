621e7ef7e59e28181da3de7ed66f586c4dfa0541
Kevin Blair <kblair40@gmail.com>
2023-07-09 07:36:43 -0500

    Add connection to prod database

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,9 +11,6 @@ import { CategoryModule } from './category/category.module';
 import { PromotionModule } from './promotion/promotion.module';
 import { TextModule } from './text/text.module';
 
-console.log('\n\nprocess.env.PG_USER:', process.env.PG_USER);
-console.log('process.env.PG_PASSWORD:', process.env.PG_PASSWORD);
-
 const devConfig: PostgresConnectionOptions = {
   type: 'postgres',
   port: 5432,
@@ -39,10 +36,7 @@ const pgConfig: PostgresConnectionOptions =
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      ...pgConfig,
-      autoLoadEntities: true,
-    }),
+    TypeOrmModule.forRoot({ ...pgConfig, autoLoadEntities: true }),
     ProductModule,
     CategoryModule,
     PromotionModule,
